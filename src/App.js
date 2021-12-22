639b0f668f638d185d7025ba3ca688eba623a19f
Viraj-Madhushan <viraj.madhushan.971210@gmail.com>
2021-12-22 14:22:21 +0530

    Import image list also

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import './App.css';
+import Data from './data.json';
+import IMAGES from './images';
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <div className="post">
+        {Data.map((post) => {
+          return (
+            <div key={post.id}>
+              <h3>{post.name}</h3>
+              <p>{post.content}</p>
+            </div>
+          );
+        })}
+      </div>
+
+      <img src={IMAGES.img1} alt="" />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/images/index.js
@@ -0,0 +1,9 @@
+const IMAGES = {
+  img1: require('./myweb1T.png'),
+  img2: require('./myweb2T.png'),
+  img3: require('./myweb3T.png'),
+  img4: require('./myweb4T.png'),
+  img5: require('./myweb5.png'),
+};
+
+export default IMAGES;
